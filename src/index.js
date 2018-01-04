d5db24dd0018659628ad0f0e68319ce0399e117e
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2018-01-04 13:05:24 +0900

    全コードを入力.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import todoApp from './reducers'
+import App from './components/App'
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(todoApp)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+)
 
